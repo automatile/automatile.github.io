1b5c137a6bad20e62edb4b29a555b16fff4ebc51
jess1dev <79570545+jess1dev@users.noreply.github.com>
2021-07-05 12:32:34 +0100

    changed image paths to relative

--- cardLoader.js
+++ cardLoader.js
@@ -17,7 +17,7 @@ async function init()
 	{
 		var card = cards[i];
 		card.getElementsByClassName("cardtitle")[0].innerHTML = objectData.objects[trSqObjs[i]].name;
-		card.style.background = "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8)), url(./img/" + trSqObjs[i] + ".jpg)";
+		card.style.background = "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.8)), url(img/" + trSqObjs[i] + ".jpg)";
 		card.style.backgroundPosition = "center top";
 		card.style.backgroundRepeat = "no-repeat";
 		card.style.backgroundSize = "cover";
